3b1eca4dd85b5dd9ac2320d1540a2989d8cc9041
Ryomanba <datemanba3@gmail.com>
2021-12-23 18:44:58 +0900

    refactor: main.py

--- main.py
+++ main.py
@@ -3,17 +3,28 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 class my_nba:
+    ID_LEBRON  = 2544
+    ID_IVERSON = 947
+    ID_ZION    = 1629627
+    NB_DRAFT   = 59
+
     def __init__(self):
-        self.pd = None
-        self.index = []
+        self.pd      = None
+        self.index   = []
+        self.score   = []
         self.players = []
-        self.score = []
 
     def get_info(self, id):
+        """
+        選手情報を取得
+        """
         player_info = commonplayerinfo.CommonPlayerInfo(player_id=id)
         self.pd = player_info.player_headline_stats.get_data_frame()
 
     def get_players_info(self, start, end):
+        """
+        指定された範囲の選手情報をインスタンス変数に追加していく
+        """
         index = 1
         for i in range(start, end):
             try:
@@ -21,16 +32,12 @@ class my_nba:
             except:
                 continue
             try:
-                ast   = self.pd['AST'][0]
                 pts   = self.pd['PTS'][0]
+                ast   = self.pd['AST'][0]
                 reb   = self.pd['REB'][0]
                 name  = self.pd['PLAYER_NAME'][0]
             except IndexError:
                 continue
-
-            # Debug
-            print(f'''name = {name},pts  = {pts}, ast  = {ast}, reb  = {reb}''')
-
             self.players.insert(0,name)
             self.index.append(index)
             index += 1
@@ -38,16 +45,18 @@ class my_nba:
             self.score.insert(0,score)
 
     def plot_bar_graph(self, year):
+        """
+        横棒グラフを表示する
+        """
         plt.barh(self.index, self.score, align="center")
         plt.yticks(self.index, self.players)
         plt.title(str(year) + " Draft Players Score")
         plt.show()
 
-# 947 = Iverson, 2544 = Lebron, 1629627 = Zion
 def main():
     print("Please wait...")
     nba = my_nba()
-    nba.get_players_info(947, (947 + 58))
+    nba.get_players_info(nba.ID_IVERSON, (nba.ID_IVERSON + nba.NB_DRAFT))
     nba.plot_bar_graph(1996)
 
 if __name__ == '__main__':
